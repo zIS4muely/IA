c16147417022bd37919e82816e055466dd8980fc
zIS4muely <106540014+zIS4muely@users.noreply.github.com>
2024-06-04 16:19:59 -0300

    AT

---
+++ script.js
@@ -0,0 +1,15 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultados');
+
+const perguntas= [{
+    enunciado:"Pergunta 1",
+    alternativas:[ "alternativa 1",
+     "alternativa 2"
+
+    ]
+
+}
+]
