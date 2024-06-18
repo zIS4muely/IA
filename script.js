e5fc530be961fca18b2f792ea90517b5a8ad8759
zIS4muely <106540014+zIS4muely@users.noreply.github.com>
2024-06-18 16:15:04 -0300

    at

--- script.js
+++ script.js
@@ -2,14 +2,51 @@ const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
-const textoResultado = document.querySelector('.texto-resultados');
+const textoResultado = document.querySelector('.texto-resultado');
 
-const perguntas= [{
-    enunciado:"Pergunta 1",
-    alternativas:[ "alternativa 1",
-     "alternativa 2"
+const perguntas = [
+    {
+        enunciado: "Qual é o maior planeta do Sistema Solar?",
+        alternativas: ["Júpiter",//certa
+            "Marte"
+        ]
 
-    ]
+    },
+    {
+        enunciado: "Qual destes é um mamífero marinho?",
+        alternativas: ["Golfinho ", //certa
+            "Tubarão"
+        ]
 
+    },
+    {
+        enunciado: "Qual é a capital da Austrália?",
+        alternativas: ["Sydney",
+            "Canberra" //certa
+        ]
+
+    },
+    {
+        enunciado: "Qual destes é um planeta anão?",
+        alternativas: ["Marte",
+            "Plutão" //certa
+        ]
+
+    },
+    {
+        enunciado: "Qual é o maior mamífero terrestre atualmente?",
+        alternativas: ["Elefante africano", //certa
+            "Urso polar"
+        ]
+
+    } // fecha objeto de pergunta
+]; //fecha lista de perguntas
+
+let atual= 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+ caixaPerguntas.textContent=perguntaAtual.enunciado;
 }
-]
+mostraPergunta();
